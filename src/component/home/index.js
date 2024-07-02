85722613abb4f39221c45a363e8ad8525410a4cd
Clemence Bricout <markerbic3@gmail.com>
2024-07-02 21:43:22 +0200

    feat(view): ajout premiere card

--- src/component/home/index.js
+++ src/component/home/index.js
@@ -3,7 +3,7 @@ import './home.css';
 import Typewriter from './Typewriter.js';
 import DivAnimee from './divAnimee/divAnimee.js';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars, faEyeSlash, faXmark } from "@fortawesome/free-solid-svg-icons";
+import { faBars, faEyeSlash, faXmark, faGear } from "@fortawesome/free-solid-svg-icons";
 
 import imageessai from "/Users/clemence/Documents/site_web/react-first-project/src/images/laptop-2557574.jpg";
 const Home = () => {
@@ -36,55 +36,47 @@ const Home = () => {
             </DivAnimee>
         </div>
         <div className='page2'>
-        <DivAnimee> 
-            <div className='card'>
-                <div>Integration</div>
-                <div>Hegeoma prépare, installe et déploie vos Mac, iPhone ou iPad, Chromebook, PC, NAS Synology ou tablettes Android
-                    dans votre infrastructure actuelle ou à venir.
-                    Nous intégrons également en environnement hétérogène.
-                    Hegeoma peut travailler au forfait et sait s’adapter à votre budget.
+            <div className='slide-container'>
+                <div className='slide-content'>
+                    <div className='card-wrapper'>
+                        <div className='image-content'>
+                            <span className='overlay'></span>
+                            <div className='cards'>
+
+                                <div className='integration'>
+                                    <div className='divimage'>
+                                        <div className='image'>
+                                            <FontAwesomeIcon icon={faGear} style={{ color: "#205883" }} size="2x" />
+                                        </div>
+                                    </div>
+                                <div className='text'>
+                                    <h2>Intégration</h2>
+                                    <p>Hegeoma prépare, installe et déploie vos Mac, iPhone ou iPad, Chromebook</p>
+                                    <button className='button'>Voir plus</button>
+                                </div>
+                                
+                                    
+                                </div>
+
+                                <div className='integration'>
+                                    <div className='divimage'>
+                                        <div className='image'>
+                                            <FontAwesomeIcon icon={faGear} style={{ color: "#205883" }} size="2x" />
+                                        </div>
+                                    </div>
+                                <div className='text'>
+                                    <h2>Intégration</h2>
+                                    <p>Hegeoma prépare, installe et déploie vos Mac, iPhone ou iPad, Chromebook</p>
+                                    <button className='button'>Voir plus</button>
+                                </div>
+                                
+                                    
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                </div>
-        </DivAnimee>
-        <DivAnimee> 
-            <div className='card'>
-                <div>Support</div>
-                <div>Hegeoma peut prendre en charge le support de tout ou partie de votre parc informatique, y compris vos iPhone, iPad ou Mac, Chromebook ou tablettes Android en situation de mobilité.</div>
             </div>
-        </DivAnimee>
-        <DivAnimee> 
-            <div className='card'>
-                <div>Formation</div>
-                <div>Vous cherchez une formation sur mesure, matérielle (Mac, iPhone, iPad) ou logicielle (OS X, iOS, intégration dans un environnement Windows sous Active Directory) ?
-                    Une formation pour un logiciel donné ? Une formation certifiante ?
-                    Hegeoma vous propose ses services en inter ou intra entreprise.</div>
-            </div>
-            </DivAnimee>
-            <DivAnimee> 
-            <div className='card'> 
-                <div>Mobilité et gestion de flotte</div>
-                <div>Vous désirez gérer et déployer plus facilement votre flotte d’appareils avec un MDM ? Pour Mac, iPhone, iPad, PC, tablettes Android ou Chromebook ?
-                Hegeoma vous accompagne.</div>
-                </div>
-            </DivAnimee>
-            <DivAnimee> 
-            <div className='card'>
-                <div>Maintenance</div>
-                <div>Vous souhaitez que quelqu’un vous conseille,
-assure la bonne évolution matérielle et logicielle de votre parc informatique ?
-Vous souhaitez faire évoluer certains Mac, iPhone, iPad, Chromebook ou PC  ?
-Hegeoma vous propose ses services.</div>
-                </div>
-            </DivAnimee>
-            <DivAnimee> 
-            <div className='card'>
-                <div>Audit et conseil</div>
-                <div>Vous désirez un conseil indépendant ?
-Un audit indépendant effectué par un professionnel certifié Apple ?
-Vous souhaitez un audit de sécurité pour vos solutions Apple ?
-Hegeoma vous propose ses services.</div>
-                </div>
-            </DivAnimee>
             
         </div>
         </div>
