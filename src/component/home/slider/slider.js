9517f9bdddbe95d25d4ad07e0d0659efa2ed299c
Clemence Bricout <markerbic3@gmail.com>
2024-07-03 16:18:29 +0200

    feat(view): slider regle 2 et 1

--- src/component/home/slider/slider.js
+++ src/component/home/slider/slider.js
@@ -1,17 +1,35 @@
 // src/components/CardSlider.js
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './cardslider.css';
 import DivAnimee from '../divAnimee/divAnimee.js';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {faChevronRight, faChevronLeft} from "@fortawesome/free-solid-svg-icons";
-
+import { faChevronRight, faChevronLeft } from "@fortawesome/free-solid-svg-icons";
 
 const CardSlider = ({ cards }) => {
     const [currentIndex, setCurrentIndex] = useState(0);
-    const cardWidth = 300; // Adjust this to the actual width of your card
+    const [cardsPerPage, setCardsPerPage] = useState(3); // Default to 3
+
+    const cardWidth = 375; // Adjust this to your card width plus margin
+
+    useEffect(() => {
+        const updateCardsPerPage = () => {
+            if (window.innerWidth < 853) {
+                setCardsPerPage(1);
+            } else if (window.innerWidth < 1200) {
+                setCardsPerPage(2);
+            } else {
+                setCardsPerPage(3);
+            }
+        };
+
+        window.addEventListener('resize', updateCardsPerPage);
+        updateCardsPerPage(); // Initial call
+
+        return () => window.removeEventListener('resize', updateCardsPerPage);
+    }, []);
 
     const goToNext = () => {
-        if (currentIndex < cards.length - 1) {
+        if (currentIndex < cards.length - cardsPerPage) {
             setCurrentIndex(currentIndex + 1);
         }
     };
@@ -24,16 +42,16 @@ const CardSlider = ({ cards }) => {
 
     return (
         <div className="slider-container">
-            <button 
-                onClick={goToPrevious} 
+            <button
+                onClick={goToPrevious}
                 className={`slider-button ${currentIndex === 0 ? 'disabled' : ''}`}
                 disabled={currentIndex === 0}
             >
                 <FontAwesomeIcon icon={faChevronLeft} style={{ color: "#7c7c7c" }} size="3x" />
             </button>
             <div className="card-container">
-                <div 
-                    className="card-slider" 
+                <div
+                    className="card-slider"
                     style={{ transform: `translateX(-${currentIndex * cardWidth}px)` }}
                 >
                     {cards.map((card, index) => (
@@ -45,10 +63,10 @@ const CardSlider = ({ cards }) => {
                     ))}
                 </div>
             </div>
-            <button 
-                onClick={goToNext} 
-                className={`slider-button ${currentIndex === cards.length - 1 ? 'disabled' : ''}`}
-                disabled={currentIndex === cards.length - 1}
+            <button
+                onClick={goToNext}
+                className={`slider-button ${currentIndex >= cards.length - cardsPerPage ? 'disabled' : ''}`}
+                disabled={currentIndex >= cards.length - cardsPerPage}
             >
                 <FontAwesomeIcon icon={faChevronRight} style={{ color: "#7c7c7c" }} size="3x" />
             </button>
