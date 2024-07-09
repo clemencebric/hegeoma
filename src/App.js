cf690393e63f95c7b07614d53a06d56e630029c7
Clemence Bricout <markerbic3@gmail.com>
2024-07-09 10:47:24 +0200

    feat(controller): mise en place url et pages differentes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import logo from './logo.svg';
 import Header from './component/header/index'
 import Footer from './component/footer/index'
 import Home from './component/home/index'
 import './App.css';
 
+const Layout = ({ children }) => {
+  return (
+    <div>
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </div>
+  );
+};
+
 function App() {
   return (
     <div className="App">
-          <div>
-            <Header />
-            <Home />
-            <Footer />
-          </div>
+      <Router>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Layout>
+    </Router>
     </div>
   );
 }
