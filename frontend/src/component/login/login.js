95d10a201f850a503e68d0d0d6e70d7aa93da5a7
Clemence Bricout <markerbic3@gmail.com>
2024-07-25 09:44:30 +0200

    feat(controller): ajout de message votre connexion a expire sur la page de login

--- frontend/src/component/login/login.js
+++ frontend/src/component/login/login.js
@@ -1,37 +1,42 @@
 import React, { useState, useContext } from 'react';
 import "./login.css";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 import { AuthContext } from '../privateroute/authcontext';
 import { post } from '../fonctions/getpost.js';
 import { getUserEmailAndStatus } from '../header/statut.js';
+
 function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
 
   const navigate = useNavigate();
+  const location = useLocation();
   const { login } = useContext(AuthContext); // Utiliser le contexte d'authentification
+
+  // Lire le message d'expiration de la connexion depuis l'état de navigation
+  const expirationMessage = location.state?.message;
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
       setErrorMessage('');
-      const response = await post('login', {email, password});
+      const response = await post('login', { email, password });
 
       if (response.token) {
         const token = response.token;
         const userEmail = response.email;
 
-        login(token, userEmail ); // Utiliser la fonction login du contexte pour gérer le token et le statut de l'utilisateur
-        
-        localStorage.setItem('token', token); //on stocke le token
+        login(token, userEmail); // Utiliser la fonction login du contexte pour gérer le token et le statut de l'utilisateur
+
+        localStorage.setItem('token', token); // on stocke le token
         const userData = getUserEmailAndStatus();
-        const statut = userData.statut; // statut de l'user connecte
+        const statut = userData.statut; // statut de l'user connecté
         if (statut === 'admin') {
-            navigate('/admin'); // Rediriger l'utilisateur vers la page d'admin
-          } else {
-            navigate('/'); // Rediriger l'utilisateur vers la page d'accueil
-          }
+          navigate('/admin'); // Rediriger l'utilisateur vers la page d'admin
+        } else {
+          navigate('/'); // Rediriger l'utilisateur vers la page d'accueil
+        }
       } else {
         setErrorMessage('Invalid credentials');
       }
@@ -41,39 +46,40 @@ function Login() {
     }
   };
 
-    return (
-        <div className="pageformulaire">
-            <div className="formulaire">
-                <div className="titreform">Login</div>
-                <form className="form" onSubmit={handleSubmit}>
-                    <div className="mb-4">
-                        <label htmlFor="email" className="">Email:</label>
-                        <input
-                            type="text"
-                            id="email"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                            className="inputlogin"
-                            placeholder='Entrez votre Email'
-                        />
-                    </div>
-                    <div className="mb-4">
-                        <label htmlFor="password" className="">Password:</label>
-                        <input
-                            type="password"
-                            id="password"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                            className="inputlogin"
-                            placeholder='Entrez votre mot de passe'
-                        />
-                    </div>
-                    <button type="submit" className="btnlogin">Login</button>
-                    {errorMessage && <p className="messageerreur">{errorMessage}</p>}
-                </form>
-            </div>
-        </div>
-    );
+  return (
+    <div className="pageformulaire">
+      <div className="formulaire">
+        <div className="titreform">Login</div>
+        <form className="form" onSubmit={handleSubmit}>
+          <div className="mb-4">
+            <label htmlFor="email" className="">Email:</label>
+            <input
+              type="text"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="inputlogin"
+              placeholder='Entrez votre Email'
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="password" className="">Password:</label>
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="inputlogin"
+              placeholder='Entrez votre mot de passe'
+            />
+          </div>
+          <button type="submit" className="btnlogin">Login</button>
+          {errorMessage && <p className="messageerreur">{errorMessage}</p>}
+          {expirationMessage && <p style={{ color: 'red' }}>{expirationMessage}</p>}
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
