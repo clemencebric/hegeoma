66cac6b65d269265066c36c0d4c53f2c6f50c0be
Clemence Bricout <markerbic3@gmail.com>
2024-08-06 14:52:15 +0200

    feat(view): titre au milieu et texte au milieu

--- frontend/src/component/services/index.js
+++ frontend/src/component/services/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import "./services.css";
 
 import integrationImage from "../../images/pexels-morningtrain-18105.jpg";
@@ -9,37 +9,35 @@ import maintenanceImage from "../../images/pexels-brett-sayles-1869322.jpg";
 import auditImage from "../../images/pexels-jourdan-wee-2023410-3644098.jpg";
 
 function Services() {
-    const [hoveredIndex, setHoveredIndex] = useState(null);
-
     const serviceDetails = [
         {
             title: "Integration",
-            description: "Hegeoma prépare, installe et déploie vos Mac, iPhone ou iPad...",
+            description: "Hegeoma prépare, installe et déploie vos Mac, iPhone ou iPad, Chromebook, PC, NAS Synology ou tablettes Android dans votre infrastructure actuelle ou à venir. Nous intégrons également en environnement hétérogène. Hegeoma peut travailler au forfait et sait s’adapter à votre budget.",
             backgroundImage: integrationImage
         },
         {
             title: "Support",
-            description: "Hegeoma peut prendre en charge le support de tout ou partie de votre parc informatique...",
+            description: "Hegeoma peut prendre en charge le support de tout ou partie de votre parc informatique, y compris vos iPhone, iPad ou Mac, Chromebook ou tablettes Android en situation de mobilité.",
             backgroundImage: supportImage
         },
         {
             title: "Formation",
-            description: "Vous cherchez une formation sur mesure, matérielle (Mac, iPhone, iPad)...",
+            description: "Vous cherchez une formation sur mesure, matérielle (Mac, iPhone, iPad) ou logicielle (OS X, iOS, intégration dans un environnement Windows sous Active Directory) ? Une formation pour un logiciel donné ? Une formation certifiante ? Hegeoma vous propose ses services en inter ou intra entreprise.",
             backgroundImage: formationImage
         },
         {
             title: "Mobilité et gestion de flotte",
-            description: "Vous désirez gérer et déployer plus facilement votre flotte d’appareils avec un MDM ?...",
+            description: "Vous désirez gérer et déployer plus facilement votre flotte d’appareils avec un MDM ? Pour Mac, iPhone, iPad, PC, tablettes Android ou Chromebook ? Hegeoma vous accompagne.",
             backgroundImage: mobiliteImage
         },
         {
             title: "Maintenance",
-            description: "Vous souhaitez que quelqu’un vous conseille, assure la bonne évolution matérielle et logicielle...",
+            description: "Vous souhaitez que quelqu’un vous conseille, assure la bonne évolution matérielle et logicielle de votre parc informatique ? Vous souhaitez faire évoluer certains Mac, iPhone, iPad, Chromebook ou PC ? Hegeoma vous propose ses services.",
             backgroundImage: maintenanceImage
         },
         {
             title: "Audit et conseil",
-            description: "Vous désirez un conseil indépendant ? Un audit indépendant effectué par un professionnel certifié Apple...",
+            description: "Vous désirez un conseil indépendant ? Un audit indépendant effectué par un professionnel certifié Apple ? Vous souhaitez un audit de sécurité pour vos solutions Apple ? Hegeoma vous propose ses services.",
             backgroundImage: auditImage
         }
     ];
@@ -49,12 +47,7 @@ function Services() {
         for (let i = 0; i < serviceDetails.length; i += 2) {
             rows.push(
                 <div className="ligne" key={i}>
-                    <div
-                        className="service"
-                        style={{ backgroundImage: `url(${serviceDetails[i].backgroundImage})` }}
-                        onMouseEnter={() => setHoveredIndex(i)}
-                        onMouseLeave={() => setHoveredIndex(null)}
-                    >
+                    <div className="service" style={{ backgroundImage: `url(${serviceDetails[i].backgroundImage})` }}>
                         <div className='grostitre'>{serviceDetails[i].title}</div>
                         <div className='fondtexte'>
                             <div className='titreligne'>{serviceDetails[i].title}</div>
@@ -62,12 +55,7 @@ function Services() {
                         </div>
                     </div>
                     {serviceDetails[i + 1] && (
-                        <div
-                            className="service"
-                            style={{ backgroundImage: `url(${serviceDetails[i + 1].backgroundImage})` }}
-                            onMouseEnter={() => setHoveredIndex(i + 1)}
-                            onMouseLeave={() => setHoveredIndex(null)}
-                        >
+                        <div className="service" style={{ backgroundImage: `url(${serviceDetails[i + 1].backgroundImage})` }}>
                             <div className='grostitre'>{serviceDetails[i + 1].title}</div>
                             <div className='fondtexte'>
                                 <div className='titreligne'>{serviceDetails[i + 1].title}</div>
