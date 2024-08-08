3f5410e071651bcf65bbdc7fd636bfe4807ddfa5
Clemence Bricout <markerbic3@gmail.com>
2024-08-08 09:51:25 +0200

    feat(view): ajout de form formspree

--- frontend/src/component/contact/contact.js
+++ frontend/src/component/contact/contact.js
@@ -1,167 +1,101 @@
-import React, { useState, useEffect } from 'react';
-import Swal from 'sweetalert2';
-import emailjs from 'emailjs-com';
-import "./contact.css";
+import React from 'react';
+import { useForm, ValidationError } from '@formspree/react';
+import './contact.css';
 
-// Initialiser EmailJS avec votre clé publique
-emailjs.init('2PP13utTMxQGvLJLl');
+function ContactForm() {
+  const [state, handleSubmit] = useForm("xdknzarw");
 
-function Contact() {
-  const [message, setMessage] = useState('');
-  const [nom, setNom] = useState('');
-  const [prenom, setPrenom] = useState('');
-  const [email, setEmail] = useState('');
-  const [telephone, setTelephone] = useState('');
-  const [captcha, setCaptcha] = useState({ num1: 0, num2: 0, answer: 0 });
-  const [userAnswer, setUserAnswer] = useState('');
-
-  useEffect(() => {
-    generateCaptcha();
-  }, []);
-
-  const generateCaptcha = () => {
-    const num1 = Math.floor(Math.random() * 10);
-    const num2 = Math.floor(Math.random() * 10);
-    const answer = num1 + num2;
-    setCaptcha({ num1, num2, answer });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    if (parseInt(userAnswer) !== captcha.answer) {
-      Swal.fire({
-        icon: 'error',
-        title: 'Captcha incorrect',
-        text: 'Veuillez réessayer.',
-      });
-      return;
-    }
-
-    // Afficher la boîte de dialogue de confirmation
-    Swal.fire({
-      title: 'Are you sure?',
-      text: "You won't be able to revert this!",
-      icon: 'warning',
-      showCancelButton: true,
-      confirmButtonColor: '#3085d6',
-      cancelButtonColor: '#d33',
-      confirmButtonText: 'Yes, submit it!'
-    }).then((result) => {
-      // Si l'utilisateur confirme, envoyer l'email
-      if (result.isConfirmed) {
-        sendEmail();
-      }
-    });
-  };
-
-  const sendEmail = () => {
-    const templateParams = {
-      from_name: `${nom} ${prenom}`,
-      from_email: email,
-      message: message,
-      telephone: telephone
-    };
-
-    emailjs.send('YOUR_SERVICE_ID', 'template_0wpxz9s', templateParams)
-      .then((response) => {
-        console.log('SUCCESS!', response.status, response.text);
-        Swal.fire({
-          icon: 'success',
-          title: 'Message submitted successfully',
-          showConfirmButton: false,
-          timer: 1500
-        });
-        setMessage('');
-        setNom('');
-        setPrenom('');
-        setEmail('');
-        setTelephone('');
-        setUserAnswer('');
-        generateCaptcha();
-      }, (error) => {
-        console.log('FAILED...', error);
-        Swal.fire({
-          icon: 'error',
-          title: 'Error submitting message',
-          text: error.text,
-        });
-      });
-  };
+  if (state.succeeded) {
+    return <p>Thanks for joining!</p>;
+  }
 
   return (
-    <div className="pagecontact">
-      <div className='minipage'>
-        <h1>Contactez-nous</h1>
-        <form className="formulairecontact" onSubmit={handleSubmit}>
-          <div className="partiedroite">
-            <label>
-              Nom:
-              <input
-                className='textareacontact premierchamp'
-                value={nom}
-                onChange={(e) => setNom(e.target.value)}
-                required
-              />
-            </label>
-            <label>
-              Prénom:
-              <input
-                className='textareacontact premierchamp'
-                value={prenom}
-                onChange={(e) => setPrenom(e.target.value)}
-                required
-              />
-            </label>
-            <label>
-              Email:
-              <input
-                className='textareacontact premierchamp'
-                type="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                required
-              />
-            </label>
-            <label>
-              Téléphone:
-              <input
-                className='textareacontact premierchamp'
-                type="tel"
-                value={telephone}
-                onChange={(e) => setTelephone(e.target.value)}
-                required
-              />
-            </label>
-            <label>
-              Message:
-              <textarea
-                className='textareacontact'
-                value={message}
-                onChange={(e) => setMessage(e.target.value)}
-                required
-              />
-            </label>
-            <div>
-              <label>
-                Captcha: {captcha.num1} + {captcha.num2} = ?
-                <input
-                  className='textareacontact'
-                  type="number"
-                  value={userAnswer}
-                  onChange={(e) => setUserAnswer(e.target.value)}
-                  required
-                />
-              </label>
-            </div>
-          </div>
-          <div className="soumettre">
-            <button type="submit">Soumettre</button>
-          </div>
-        </form>
-      </div>
+    <div className='pagecontact'>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="firstName">
+          First Name
+        </label>
+        <input
+          id="firstName"
+          type="text"
+          name="firstName"
+        />
+        <ValidationError
+          prefix="First Name"
+          field="firstName"
+          errors={state.errors}
+        />
+
+        <label htmlFor="lastName">
+          Last Name
+        </label>
+        <input
+          id="lastName"
+          type="text"
+          name="lastName"
+        />
+        <ValidationError
+          prefix="Last Name"
+          field="lastName"
+          errors={state.errors}
+        />
+
+        <label htmlFor="email">
+          Email Address
+        </label>
+        <input
+          id="email"
+          type="email"
+          name="email"
+        />
+        <ValidationError
+          prefix="Email"
+          field="email"
+          errors={state.errors}
+        />
+
+        <label htmlFor="phone">
+          Phone Number
+        </label>
+        <input
+          id="phone"
+          type="tel"
+          name="phone"
+        />
+        <ValidationError
+          prefix="Phone"
+          field="phone"
+          errors={state.errors}
+        />
+
+        <label htmlFor="message">
+          Message
+        </label>
+        <textarea
+          id="message"
+          name="message"
+        />
+        <ValidationError
+          prefix="Message"
+          field="message"
+          errors={state.errors}
+        />
+
+        <button type="submit" disabled={state.submitting}>
+          Submit
+        </button>
+      </form>
     </div>
   );
 }
 
-export default Contact;
+function App() {
+  return (
+    <div className="App">
+      <h1>Contact Form</h1>
+      <ContactForm />
+    </div>
+  );
+}
+
+export default App;
